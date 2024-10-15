9dfd7cc8b405b13a7b3e48af9d974912f38c8086
alimohamedac <a.mohamed666999@gmail.com>
2024-10-15 20:55:41 +0300

    jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\DeleteOldPosts;
+use App\Jobs\LogRandomData;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +18,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->job(new DeleteOldPosts)->daily();
+        $schedule->job(new LogRandomData)->everySixHours();
     }
 
     /**
